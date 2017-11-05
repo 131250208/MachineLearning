edc72ba7344228927175355ac0ec804628273f8c
王雨城 <wangyucheng@wangyuchengs-Mac-mini.local>
2017-11-05 12:17:30 +0800

    去掉了通过编辑距离计算相似密码的过程，换成了全等，节省时间

--- application/pswd_strength/SecurityAnalysis.py
+++ application/pswd_strength/SecurityAnalysis.py
@@ -84,9 +84,11 @@ class SecurityAnalysis:
             index = line.split(":")[0]
             code = line.split(":")[1].strip()
 
-            if self.get_simularity_degree_pws(pswd, code) >= 0.7: # 如果两个密码相似度大于等于0.7，将它们归为一个相似密码类
+            # if self.get_simularity_degree_pws(pswd, code) >= 0.7: # 如果两个密码相似度大于等于0.7，将它们归为一个相似密码类
+            #     s_num += 1
+            #     print(u"正在比对密码库,计算相似密码数作为其中一个输入特征...相似密码：index: %s, pw: %s" % (index,code))
+            if pswd == code:
                 s_num += 1
-                print(u"正在比对密码库,计算相似密码数作为其中一个输入特征...相似密码：index: %s, pw: %s" % (index,code))
 
         return s_num # 返回相似密码类的总数
 
@@ -125,6 +127,6 @@ if __name__=="__main__":
     # plt.show()
 
     sa = SecurityAnalysis()
-    print(u"预测结果是：y = %s " % sa.analyse_security("Ddmpcuxc.")) # 弱密码示例：password
+    print(u"预测结果是：y = %s " % sa.analyse_security("password")) # 弱密码示例：password  强密码：Ddmpcuxc.
 
 
